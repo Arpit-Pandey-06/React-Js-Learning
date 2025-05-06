6cb8a5995dd7f98f22d68b8bbbcaa0c2c731ef2d
Arpit-Pandey-06 <pandeyarpit5441@gmail.com>
2025-05-06 17:48:24 +0530

    Practice Change

--- 03tailwindcssprop/src/App.jsx
+++ 03tailwindcssprop/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Card.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let usernames=["Jordan","One8","Nike"]
   return (
     <>
-    <h1 className='bg-blue-200 text-black rounded-2xl p-1' >Tailwind Test</h1>
+    <h1 className='bg-blue-200 text-black rounded-2xl p-1 mb-4' >Tailwind Test</h1>
+    {usernames.map((name,index)=>(
+      <Card key={index} username={name}/>
+    ))}
     </>
   )
 }
